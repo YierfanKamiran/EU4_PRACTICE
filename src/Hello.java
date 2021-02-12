bacc219dc1c2a63672580450882f02e207145727
YierfanKamiran <YierfanKamiran@users.noreply.github.com>
2021-02-12 15:53:25 +0300

    added some words

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,7 @@
 public class Hello {
     //more stuff
     // i am happy studying in Cybertek
+    //i want to add new messages in my code
+    //should i share it again on github
+    //i dont think so
 }
